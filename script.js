0f3fb81c0ab797c6895f820b05c7eec0577de5db
@chavidev <javiercastaneda303@gmail.com>
2021-10-13 11:56:10 +0100

    vuelta a empezar con las tarjetas

--- script.js
+++ script.js
@@ -1,81 +1,40 @@
 let tabla = document.getElementById("tabla");
-let jornadas = [] // contiene las jornadas
+let creandoTabla = "";
+let jornada = "";
+let equipoLocal = "";
+let equipoVisitante = "";
+let resultado = "";
+ 
+ 
+//console.log(data.matches[4])
+console.log(data.matches)
+ 
+
+//document.write(`Local: ${data.matches[0].homeTeam.name} ${data.matches[0].score.fullTime.homeTeam}   -   ${data.matches[0].score.fullTime.awayTeam} Visitante: ${data.matches[0].awayTeam.name}`)
+//console.log(data.lenght)
+console.log(data.matches.length)
+for (i=0 ; i<data.matches.length ; i++ ){
+  jornada = data.matches[i].matchday;
+  equipoLocal = data.matches[i].homeTeam.name;
+  equipoVisitante = data.matches[i].awayTeam.name;
+  puntosLocal = data.matches[i].score.fullTime.homeTeam;
+  puntosVisitante = data.matches[i].score.fullTime.awayTeam
+ 
+  creandoTabla += jornada + "Local:" + equipoLocal + puntosLocal + "-"+   puntosVisitante + "Visitante:" + equipoVisitante +  "<br>";
+  //funciona
+  //document.write(`${data.matches[i].matchday} Local: ${data.matches[i].homeTeam.name} ${data.matches[i].score.fullTime.homeTeam}   -   ${data.matches[i].score.fullTime.awayTeam} Visitante: ${data.matches[i].awayTeam.name} \n <br>`)
+}
  
 /*
  
-jornadas = [
-  '1': [
-    {equipo1: '', equipo2: '', punto1: '', puntos2: ''},
-    {equipo1: '', equipo2: '', punto1: '', puntos2: ''},
-    {equipo1: '', equipo2: '', punto1: '', puntos2: ''}
-    ...
-  ],
-  '2': [
-    {equipo1: '', equipo2: '', punto1: '', puntos2: ''},
-    {equipo1: '', equipo2: '', punto1: '', puntos2: ''},
-    {equipo1: '', equipo2: '', punto1: '', puntos2: ''}
-    ...
-  ],
-  ...
-]
+let inyeccion = ${data.matches[i].matchday} Local: ${data.matches[i].homeTeam.name} ${data.matches[i].score.fullTime.homeTeam}   -   ${data.matches[i].score.fullTime.awayTeam} Visitante: ${data.matches[i].awayTeam.name} \n <br>;
+ */
  
-*/
+//tabla.innerHTML = "hola colega hola colega" +"<br>"+ "2 hola colega"+data.matches[0].matchday;
  
-function llenarArrayJornadas() {
-  let jornada = 0
-  let jornadaAnterior = 0
-  let partido = {}
- 
-  for (i=0 ; i < data.matches.length ; i++ ){
-    jornada = data.matches[i].matchday //
-    partido = {
-      equipoLocal: data.matches[i].homeTeam.name,
-      equipoVisitante: data.matches[i].awayTeam.name,
-      puntosLocal: data.matches[i].score.fullTime.homeTeam,
-      puntosVisitante: data.matches[i].score.fullTime.awayTeam
-    }
-    // misma jornada pero se agrega un partido
-    if(jornada === jornadaAnterior) {
-      jornadas[`${jornada}`].push(partido)
-    }
-    // jornada nueva
-    else {
-      jornadas[`${jornada}`] = [partido]
-    }
-    jornadaAnterior = data.matches[i].matchday  
-  }
-}
- 
-function renderizarJornadas() {
-  for(let i = 1; i<jornadas.length; i++) {
-    let numeroDeJornada = i
-    let tarjetaJornada = `
-    <div class="jornada">
-      <h1 class="numero-jornada">Jornada #${numeroDeJornada}</h1>
-      <div class="cab">
-        <h2>Local</h2>
-        <h2>Resultado</h2>
-        <h2>Visitante</h2>
-      </div>
-      <div class="equipos" id="equipos-jornada-${numeroDeJornada}">
-         
-      </div>
-    </div>
-    `
-    tabla.innerHTML += tarjetaJornada;
-    for(let j = 0; j < jornadas[`${numeroDeJornada}`].length; j++) {
-      document.getElementById(`equipos-jornada-${numeroDeJornada}`).innerHTML += `
-      <div class="partido">
-        <h3>${jornadas[`${numeroDeJornada}`][j].equipoLocal}</h3>
-        <h3>${jornadas[`${numeroDeJornada}`][j].puntosLocal} - ${jornadas[`${numeroDeJornada}`][j].puntosVisitante}</h3>
-        <h3>${jornadas[`${numeroDeJornada}`][j].equipoVisitante}</h3>
-      </div>
-      `
-    }
-  }
-}
- 
-llenarArrayJornadas()
-renderizarJornadas()
+//tabla.innerHTML = data.matches[0].matchday + "Local:" + data.matches[0].homeTeam.name + data.matches[0].score.fullTime.homeTeam + " - " +data.matches[0].score.fullTime.awayTeam + "Visitante:" + data.matches[0].awayTeam.name + "<br>";
  
  
+//tabla.innerHTML = `${data.matches[0].matchday} Local: ${data.matches[0].homeTeam.name} ${data.matches[0].score.fullTime.homeTeam}   -   ${data.matches[0].score.fullTime.awayTeam} Visitante: ${data.matches[0].awayTeam.name} <br>`
+ 
+tabla.innerHTML = creandoTabla;
