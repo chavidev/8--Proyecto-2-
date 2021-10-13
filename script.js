3a51244c5d3e48d6b3ade1dd3bfad7de084b559d
@chavidev <javiercastaneda303@gmail.com>
2021-10-13 14:01:03 +0100

    _

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let tabla = document.getElementById("tabla");
+// https://codepen.io/susanwinters/pen/YpRMdg
+//let partidos = document.createElement("tbody");
+let partidos = document.createElement("h1"); 
+
+
+//let tabla = document.getElementById("tabla");
 let creandoTabla = "";
 let jornada = "";
 let equipoLocal = "";
@@ -6,8 +11,8 @@ let equipoVisitante = "";
 let resultado = "";
  
  
-//console.log(data.matches[4])
-console.log(data.matches)
+
+//console.log(data.matches)
  
 
 //document.write(`Local: ${data.matches[0].homeTeam.name} ${data.matches[0].score.fullTime.homeTeam}   -   ${data.matches[0].score.fullTime.awayTeam} Visitante: ${data.matches[0].awayTeam.name}`)
@@ -22,9 +27,21 @@ for (i=0 ; i<data.matches.length ; i++ ){
   puntosVisitante = data.matches[i].score.fullTime.awayTeam
  
   //creandoTabla += jornada + "Local:" + equipoLocal + puntosLocal + "-"+   puntosVisitante + "Visitante:" + equipoVisitante +  "<br>";
-  creandoTabla +=`${data.matches[i].matchday} Local: ${data.matches[i].homeTeam.name} ${data.matches[i].score.fullTime.homeTeam}   -   ${data.matches[i].score.fullTime.awayTeam} Visitante: ${data.matches[i].awayTeam.name} \n <br>`
-  //funciona
-  //document.write(`${data.matches[i].matchday} Local: ${data.matches[i].homeTeam.name} ${data.matches[i].score.fullTime.homeTeam}   -   ${data.matches[i].score.fullTime.awayTeam} Visitante: ${data.matches[i].awayTeam.name} \n <br>`)
+  
+  //creandoTabla +=`${data.matches[i].matchday} Local: ${data.matches[i].homeTeam.name} ${data.matches[i].score.fullTime.homeTeam}   -   ${data.matches[i].score.fullTime.awayTeam} Visitante: ${data.matches[i].awayTeam.name} \n <br>`
+  /* creandoTabla+=`<tr>
+  <td>${data.matches[i].homeTeam.name}</td>
+  <td>${data.matches[i].score.fullTime.homeTeam}   -   ${data.matches[i].score.fullTime.awayTeam}</td>
+  <td>${data.matches[i].awayTeam.name}</td>
+  </tr>`
+  */
+  creandoTabla += "<tr><td>local</td>  <td>resultado</td>  <td>visitante</td>  </tr>"
 }
  
-tabla.innerHTML = creandoTabla;
+//tabla.innerHTML = creandoTabla;
+
+//document.tbody.appendChild(div)
+//partidos.innerHTML = creandoTabla;
+//document.tbody.append(div)
+
+partidos.append(creandoTabla);
