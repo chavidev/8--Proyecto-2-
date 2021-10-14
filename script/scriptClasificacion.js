22870893ab0f1d9cd607a5295670038609af1d5b
@chavidev <javiercastaneda303@gmail.com>
2021-10-14 14:34:09 +0100

    ## solucionado object object con JSON.stringify(Object)

--- script/scriptClasificacion.js
+++ script/scriptClasificacion.js
@@ -1,4 +1,5 @@
 console.log("clasificación");
-let variable1 = `${dataClasificacion.standings}`;
+let variable1 = JSON.stringify(dataClasificacion.standings[0].table[2].position);
 console.log(variable1);
 document.write(`${variable1}`);
+
