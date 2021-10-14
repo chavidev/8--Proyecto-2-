f7ba0f4f4a2ea85ec79fd716db7198a2a23ad24f
@chavidev <javiercastaneda303@gmail.com>
2021-10-14 16:32:45 +0100

    ___modificaciones en clasificación

--- script/scriptClasificacion.js
+++ script/scriptClasificacion.js
@@ -1,5 +1,33 @@
-console.log("clasificación");
-let variable1 = JSON.stringify(dataClasificacion.standings[0].table[2].position);
-console.log(variable1);
-document.write(`${variable1}`);
+//console.log("clasificación");
+//let variable1 = JSON.stringify(dataClasificacion.standings[0].table[2].position);
+//console.log(variable1);
+//document.write(`${variable1}`);
 
+function verClasificacion(){
+  for(let i = 0 ;i<dataClasificacion.standings[0].table.length;i++){
+    let arrayTd , posicion , teamName , teamId , teamLogo , partidosJugados , victorias , empates , derrotas , golesFaborables , golesContra , golesDiferencia , puntos;
+    posicion = dataClasificacion.standings[0].table[i].position;
+    teamName = dataClasificacion.standings[0].table[i].team.name;
+    teamId = dataClasificacion.standings[0].table[i].team.id;
+    teamLogo = dataClasificacion.standings[0].table[i].team.crestUrl;
+    partidosJugados = dataClasificacion.standings[0].table[i].playedGames;
+    victorias = dataClasificacion.standings[0].table[i].won;
+    empates = dataClasificacion.standings[0].table[i].draw;
+    derrotas = dataClasificacion.standings[0].table[i].lost;
+    golesFaborables = dataClasificacion.standings[0].table[i].goalsFor;
+    golesContra = dataClasificacion.standings[0].table[i].goalsAgainst;
+    golesDiferencia = dataClasificacion.standings[0].table[i].goalDifference;
+    puntos = dataClasificacion.standings[0].table[i].points;
+    
+    arrayTd = {posicion , teamName , teamId , teamLogo , partidosJugados , victorias , empates , derrotas , golesFaborables , golesContra , golesDiferencia , puntos};
+    inyectarTd(arrayTd);
+  }
+}
+
+function inyectarTd(arrayTd){  
+  //let clasificacion = document.querySelector("tbody");
+  console.log(arrayTd)
+
+}
+
+verClasificacion()
