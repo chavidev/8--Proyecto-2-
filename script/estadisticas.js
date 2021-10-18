3d9e647fe11c1114e33da5e60e80783ec62d3ff4
@chavidev <javiercastaneda303@gmail.com>
2021-10-18 12:47:41 +0100

    &&&& no entiendo por qué aparece mas de veinte veces la inyección del objeto

--- script/estadisticas.js
+++ script/estadisticas.js
@@ -1,67 +1,68 @@
-//console.log("hola colega");
-calcularEstadisticas ()
-//console.log(calcularEstadisticas ())
-function calcularEstadisticas (){
-  let medirGoles = {}
+/* 
+{
+  id:
+  nombre:
+  totalGolesMarcados:
+  golesRecibidosFuera:
+  avg:
+}
+ */
+let arrayGoles = [];
+
+contarGoles ();
+console.log(arrayGoles)
+
+function contarGoles (){  
   for(let i=0 ; i<data.matches.length ;i++){
+    let jornada, equipoLocal, idEquipoLocal, equipoVisitante, idEquipoVisitante, golesLocal, golesVisitante  
     jornada = data.matches[i].matchday;
     equipoLocal = data.matches[i].homeTeam.name;
-    idEquipoLocal = data.matches[i].homeTeam.id.toString();
+    idEquipoLocal = data.matches[i].homeTeam.id;
     equipoVisitante = data.matches[i].awayTeam.name;
-    idEquipoVisitante = data.matches[i].awayTeam.id.toString();
+    idEquipoVisitante = data.matches[i].awayTeam.id;
     golesLocal = data.matches[i].score.fullTime.homeTeam;
-    golesVisitante = data.matches[i].score.fullTime.awayTeam
-  
-    //document.write(`${jornada } ${equipoLocal}${golesLocal}<br>`)               
-    //document.write("hola colega")
-    //console.log("hola colega")
-    //console.log(data);
-    //   &&&&medirGoles[idEquipoLocal] += golesLocal ;
-    //medirGoles[idEquipoLocal].partidos += 1 ;
-    //medirGoles[idEquipoVisitante] += golesVisitante ;
-    //   &&&&medirGoles[idEquipoVisitante].partidos += 1 ;
-    //typeof(golesLocal)
-    //console.log(golesLocal)
-
-    //if (medirGoles[idEquipoLocal].goles !== undefined ){
-    //if(typeof(medirGoles[idEquipoLocal].goles) === number){
-    
-    
-    //pendiente de finalizar con hasOwnProperty 
-/*     const obj = { answer: 42 };
-'answer' in obj; // true
-obj.hasOwnProperty('answer'); // true
- */
-
-//solución pasar los números de las keys a strings
-
-    if((medirGoles[idEquipoLocal].hasOwnProperty(idEquipoLocal))){
-      medirGoles[idEquipoLocal] = {goles:golesLocal}
+    golesVisitante = data.matches[i].score.fullTime.awayTeam  
+    /* 
+    console.log(`
+      jornada=${jornada}
+      equipoLocal=${equipoLocal}
+      golesLocal=${golesLocal}         
+    `)
+     */
+    if(golesLocal === null){
+      console.log("Ups con los null paso de hacer nada")
     } else {
-      if (medirGoles[idEquipoLocal].goles >= 0){
-        let golesTotal = 0
-        let acumulados = medirGoles[idEquipoLocal].goles ;
-        golesTotal = medirGoles[idEquipoLocal].goles + golesLocal ;
-        //medirGoles[idEquipoLocal] = golesTotal ;
-        medirGoles[idEquipoLocal].goles += golesLocal
-        console.log(`
-          jornada=${jornada}
-          equipoLocal=${equipoLocal}
-          golesLocal=${golesLocal}
-          acumulados=${acumulados}  
-          golesTotal=${golesTotal} 
-          medirGoles[idEquipoLocal].goles=${medirGoles[idEquipoLocal].goles} \n \n \n
-        `)
-      } 
-    }
-  }
-  
+      recorrerArrayGoles({"id": idEquipoLocal, "nombre":equipoLocal, "golesPartido":golesLocal});
+    } 
+      /* 
+        acumulados=${acumulados}  
+        golesTotal=${golesTotal} 
+        medirGoles[idEquipoLocal].goles=${medirGoles[idEquipoLocal].goles} \n \n \n  
+      */
+  }  
 }
 
+function recorrerArrayGoles(Obj){
+  for (let j = 0; j < arrayGoles.length ; j++){
+    if ((arrayGoles[j].id === Obj.id)){
+      arrayGoles[j].totalGolesMarcados += Obj.golesPartido
+      //no entiendo por qué aparece mas de 20 veces
+      console.log("antes del retunr, estoy inyectando un nuevo objeto")
+      return
+      console.log("después del return: ALGO ESTÁ FALLANDO COLEGA") //
+    }    
+  }
+  if (Obj.golesPartido === null ){
+    console.log("ups, te está entrando un nulo que no devería, pero aquí lo paro por si acaso")
+  } else {
+    arrayGoles.push({
+      "id": Obj.id ,
+      "nombre": Obj.nombre ,
+      "totalGolesMarcados":Obj.golesPartido,
+      "golesRecibidosFuera":"",
+      "avg":""
+    })
+  }
 
-
-
-
-//console.log(data);
-
-//console.log(data.matches.length)
+  //console.log(JSON.stringify(Obj));
+}
