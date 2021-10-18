5e31c4cfad305828ff578e4071724a47a5a7de8b
@chavidev <javiercastaneda303@gmail.com>
2021-10-18 10:24:39 +0100

    &&&&& no consigo sacarlo if de undefined tengo que pasar las keys a string Ej:'45'

--- script/estadisticas.js
+++ script/estadisticas.js
@@ -2,13 +2,13 @@
 calcularEstadisticas ()
 //console.log(calcularEstadisticas ())
 function calcularEstadisticas (){
-  let mediaGoles = {}
+  let medirGoles = {}
   for(let i=0 ; i<data.matches.length ;i++){
     jornada = data.matches[i].matchday;
     equipoLocal = data.matches[i].homeTeam.name;
-    idEquipoLocal = data.matches[i].homeTeam.id;
+    idEquipoLocal = data.matches[i].homeTeam.id.toString();
     equipoVisitante = data.matches[i].awayTeam.name;
-    idEquipoVisitante = data.matches[i].awayTeam.id;
+    idEquipoVisitante = data.matches[i].awayTeam.id.toString();
     golesLocal = data.matches[i].score.fullTime.homeTeam;
     golesVisitante = data.matches[i].score.fullTime.awayTeam
   
@@ -16,38 +16,51 @@ function calcularEstadisticas (){
     //document.write("hola colega")
     //console.log("hola colega")
     //console.log(data);
-    mediaGoles[idEquipoLocal] += golesLocal ;
-    //mediaGoles[idEquipoLocal].partidos += 1 ;
-    mediaGoles[idEquipoVisitante] += golesVisitante ;
-    //mediaGoles[idEquipoVisitante].partidos += 1 ;
+    //   &&&&medirGoles[idEquipoLocal] += golesLocal ;
+    //medirGoles[idEquipoLocal].partidos += 1 ;
+    //medirGoles[idEquipoVisitante] += golesVisitante ;
+    //   &&&&medirGoles[idEquipoVisitante].partidos += 1 ;
     //typeof(golesLocal)
     //console.log(golesLocal)
-    if (typeof(golesLocal) === 'number' ){
-      //console.log(golesLocal)
 
-      if (mediaGoles[idEquipoLocal] > 0){
+    //if (medirGoles[idEquipoLocal].goles !== undefined ){
+    //if(typeof(medirGoles[idEquipoLocal].goles) === number){
+    
+    
+    //pendiente de finalizar con hasOwnProperty 
+/*     const obj = { answer: 42 };
+'answer' in obj; // true
+obj.hasOwnProperty('answer'); // true
+ */
+
+//solución pasar los números de las keys a strings
+
+    if((medirGoles[idEquipoLocal].hasOwnProperty(idEquipoLocal))){
+      medirGoles[idEquipoLocal] = {goles:golesLocal}
+    } else {
+      if (medirGoles[idEquipoLocal].goles >= 0){
         let golesTotal = 0
-        let acumulados = mediaGoles[idEquipoLocal] ;
-        golesTotal = mediaGoles[idEquipoLocal] + golesLocal ;
-        //mediaGoles[idEquipoLocal] = golesTotal ;
-        mediaGoles[idEquipoLocal] += golesLocal
+        let acumulados = medirGoles[idEquipoLocal].goles ;
+        golesTotal = medirGoles[idEquipoLocal].goles + golesLocal ;
+        //medirGoles[idEquipoLocal] = golesTotal ;
+        medirGoles[idEquipoLocal].goles += golesLocal
         console.log(`
-          jornada=${jornada} 
-          
+          jornada=${jornada}
           equipoLocal=${equipoLocal}
           golesLocal=${golesLocal}
           acumulados=${acumulados}  
           golesTotal=${golesTotal} 
-          mediaGoles[idEquipoLocal]=${mediaGoles[idEquipoLocal]} \n \n \n
+          medirGoles[idEquipoLocal].goles=${medirGoles[idEquipoLocal].goles} \n \n \n
         `)
-      } else {
-        mediaGoles[idEquipoLocal] = golesLocal
-      }
-      
+      } 
     }
   }
-  return mediaGoles
+  
 }
+
+
+
+
 
 //console.log(data);
 
